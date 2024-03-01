f266ec3327ff9d8f61dcd14b2b9c3a3b31de1051
felix.hsu <felix.hse@kenkone.com>
2024-03-01 18:20:51 +0800

    add new database

--- pages/api/notion.ts
+++ pages/api/notion.ts
@@ -1,16 +1,11 @@
 import { Client } from '@notionhq/client';
 import { NextApiRequest, NextApiResponse} from 'next';
 
-const notionSecret =process.env. NOTION_TOKEN
+const notionSecret = process.env. NOTION_TOKEN
 const notionDatabaseID = process.env.NOTION_DATABASE_ID
 
 const notion = new Client({ auth: notionSecret })
 
-type Row = {
-    first_name: { id: string, rich_test: { text:{ content: string } }[] }
-    link_to_yt: { id: string, url: string }
-    name: { id: string, title: { text: { content: string } }[] }
-}
 
 export default async function handler(req: NextApiRequest,res: NextApiResponse) {
     if (!notionSecret || !notionDatabaseID) throw new Error('Missing notion seret or DB_ID.')
@@ -18,11 +13,24 @@ export default async function handler(req: NextApiRequest,res: NextApiResponse) 
     const query = await notion.databases.query({
         database_id: notionDatabaseID,
     })
+
+    const data = {}
     
     // @ts-ignore
     const rows = query.results.map((res) => res.properties) as Row[]
 
-    // console.log('rows',rows);
 
-    res.status(200).json(rows)
+
+    const RowData = rows.map(row => ({
+        "id": 1,
+        "date": "0309",
+        "title": row.title.title[0].text.content,
+        "tags": row.Tags.multi_select.map((tag: any) => tag.name),
+        "content": row.schedule.rich_text[0].text.content.split('\n'),
+        "startTime": row.startTime.rich_text[0].text.content,
+        "icon": row.icon.rich_text[0].text.content
+    }));
+    
+    
+    res.status(200).json({ RowData })
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import Link from 'next/link';
 
 const fetchFromNotion = async () => {
-  const res = await axios.get('http://localhost:3000/api/notion')
+  const res = await axios.get('http://localhost:3001/api/notion')
   
   return res.data
 }
@@ -11,15 +12,20 @@ export default async function Home() {
   
   return (
     <div className="h-screen">
-      {
-        rows.map((item: any, index: any) => (
-          <div className="p-10">
-            <h1 className=" p-3" key={index}>{item.Name.title[0].plain_text}</h1>
-            <span key={index} className="p-3">英文團名: {item.first_name.rich_text[0].plain_text}</span>
-            <span key={index}><a href={item.link_to_yt.url} className="text-red-500">Youtube</a></span>
-          </div>
-        ))
-      }
+      <div className='flex w-3/4 m-auto py-10'>
+        {
+          rows.map((item: any, index: any) => (
+            <div className="">
+              <h1 className="" key={index}>{item.name}</h1>
+              <span key={index} className="">英文名稱: {item.firstname}</span>
+              <span key={index}><a href={item.youtubeUrl} className="text-red-500">Youtube</a></span>
+            </div>
+          ))
+        }
+      </div>
+      <h1 className="title">
+        <Link href="/post/first-page">this page!</Link>
+      </h1>
     </div>
   );
 }
